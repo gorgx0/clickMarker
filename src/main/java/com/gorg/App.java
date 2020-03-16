068592674c95d9d9f8b951a910cd560822f6f5a8
Marcin Krasowski <marcin.krasowski@gmail.com>
2020-03-16 01:20:38 +0100

    with context menu (no actions)

--- src/main/java/com/gorg/App.java
+++ src/main/java/com/gorg/App.java
@@ -7,6 +7,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -24,22 +29,28 @@ public class App extends Application {
         Pane root = new Pane();
         Scene scene = new Scene(root);
         scene.setFill(Color.web("blue",0.1));
+        root.setStyle("-fx-background-color: rgba(0,0,255,0.1)");
         Rectangle2D bounds = Screen.getPrimary().getBounds();
         Canvas canvas = new Canvas(bounds.getWidth(),bounds.getHeight());
         root.getChildren().add(canvas);
         final GraphicsContext gc = canvas.getGraphicsContext2D();
+
         scene.setOnMouseClicked(e -> {
             double sceneX = e.getSceneX();
             double sceneY = e.getSceneY();
-            double screenX = e.getScreenX();
-            double screenY = e.getScreenY();
-            gc.setLineWidth(2.0);
-            gc.setStroke(Color.RED);
-            gc.strokeOval(sceneX-15, sceneY-15,30, 30);
-            gc.setFont(Font.font("Monoid",15));
-            gc.strokeText(String.valueOf(counter.counter++),counter.counter<10?sceneX-5:sceneX-10, sceneY+6);
-            gc.strokeText(String.valueOf(sceneX), sceneX+20, sceneY-5);
-            gc.strokeText(String.valueOf(sceneY), sceneX+20, sceneY+15);
+            MouseButton button = e.getButton();
+            switch (button) {
+                case PRIMARY:
+                    addPoint(counter, gc, sceneX, sceneY);
+                    break;
+                case SECONDARY:
+                    ContextMenu contextMenu = new ContextMenu();
+                    contextMenu.getItems().add(new MenuItem("Reset points"));
+                    contextMenu.getItems().add(new MenuItem("Set relative point"));
+                    contextMenu.getItems().add(new MenuItem("Set boundaries"));
+                    contextMenu.show(primaryStage,sceneX,sceneY);
+                    break;
+            }
         });
         primaryStage.setTitle("This is primary stage");
         primaryStage.setScene(scene);
@@ -47,6 +58,16 @@ public class App extends Application {
         primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.setFullScreen(true);
         primaryStage.show();
+    }
+
+    private void addPoint(Counter counter, GraphicsContext gc, double sceneX, double sceneY) {
+        gc.setLineWidth(2.0);
+        gc.setStroke(Color.RED);
+        gc.strokeOval(sceneX-15, sceneY-15,30, 30);
+        gc.setFont(Font.font("Monoid",15));
+        gc.strokeText(String.valueOf(counter.counter++),counter.counter<10?sceneX-5:sceneX-10, sceneY+6);
+        gc.strokeText(String.valueOf(sceneX), sceneX+20, sceneY-5);
+        gc.strokeText(String.valueOf(sceneY), sceneX+20, sceneY+15);
     }
 
     public static void main(String[] args) {
